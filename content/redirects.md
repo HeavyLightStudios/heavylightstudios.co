09efbcbb2246b5ede77f53f6a8facf4ec584a0bd
Glenn Hevey <glenn.hevey@gmail.com>
2018-11-18 22:56:24 +1100

    Added initial cut at routing

--- content/redirects.md
+++
@@ -1,7 +0,0 @@
----
-Title: redirects
-Type: Page
-outputfilename: _redirects
----
-# Redirect default Netlify subdomain to primary domain
-http://zealous-einstein-986b58.netlify.com/* http://heavylightstudios.co/:splat 301!
---
+++ public/resources/js/global.js
@@ -0,0 +1,33 @@
+function getPosition(element) {
+    var e = document.getElementById(element);
+    var left = 0;
+    var top = 0;
+    do {
+        left += e.offsetLeft;
+        top += e.offsetTop;
+    } while (e = e.offsetParent);
+    return [left, top];
+}
+function jumpTo(id) {
+    if (id == 'home') {
+        window.scrollTo(0, 0);
+        history.pushState('', "" + document.title, "/");
+    }
+    else {
+        var position = getPosition(id);
+        window.scrollTo(position[0], position[1]);
+        history.pushState('', "" + document.title, "/" + id);
+    }
+}
+window.onload = function (ev) {
+    var url = location.hash.slice(1) || '/';
+    if (url === '/') {
+        jumpTo('home');
+    }
+    else if (url === 'about') {
+        jumpTo('about');
+    }
+    else if (url === 'contact') {
+        jumpTo('contact');
+    }
+};
